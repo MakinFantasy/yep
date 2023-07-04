cee958ce88aa4a2a3a99910ca35cf9b1e5fb34c9
dmitriytheprogrammer <dmitriy.only.work@mail.ru>
2023-07-04 21:31:03 +0300

    построена архитектура проекта

---
+++ vue-project/src/components/UI/index.js
@@ -0,0 +1,6 @@
+import MyButton from '@/components/UI/MyButton.vue';
+import MyInput from '@/components/UI/MyInput.vue';
+import MyTextarea from '@/components/UI/MyTextarea.vue';
+import MyCheckbox from '@/components/UI/MyCheckbox.vue';
+
+export default [MyButton, MyInput, MyTextarea, MyCheckbox];
